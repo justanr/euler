96ac888c9a9b464c46ba96f8ea3f16995f596ccd
justanr <alecreiter@gmail.com>
2015-04-26 20:19:38 -0400

    gcd needs to return a and not b.

--- 005/solution.py
+++ 005/solution.py
@@ -4,10 +4,10 @@ from functools import reduce
 def gcd(a, b):
     while b:
         a, b = b, a % b
-    return b
+    return a
 
 
 def lcm(a, b):
-    return abs(a*b)//gcd(a, b)
+    return abs(a*b)/gcd(a, b)
 
 print(reduce(lcm, range(11, 21)))
